e21b16ea8fcaf0f084a54032aec4b379a33c7c6f
kanaydo <batara.girsang@outlook.com>
2025-10-23 22:01:37 +0700

    fix: change to direct load

--- lib/ceppu.rb
+++ lib/ceppu.rb
@@ -1,12 +1,16 @@
 require 'ceppu/controller_methods'
 # require 'ceppu/railtie' if defined?(Rails)
 
-module Ceppu
-  class Engine < Rails::Engine
-    initializer 'ceppu.controller_methods' do
-      ActiveSupport.on_load(:action_controller) do
-        include Ceppu::ControllerMethods
-      end
-    end
-  end
+ActiveSupport.on_load(:action_controller) do
+  include Ceppu::ControllerMethods
 end
+
+# module Ceppu
+#   class Engine < Rails::Engine
+#     initializer 'ceppu.controller_methods' do
+#       ActiveSupport.on_load(:action_controller) do
+#         include Ceppu::ControllerMethods
+#       end
+#     end
+#   end
+# end
