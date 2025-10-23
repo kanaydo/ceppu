3868f0d89f489b74ec495b8d6e6f7a03a911eef7
kanaydo <batara.girsang@outlook.com>
2025-10-23 22:00:11 +0700

    fix: using rails engine instead of railtie

--- lib/ceppu.rb
+++ lib/ceppu.rb
@@ -1,2 +1,12 @@
 require 'ceppu/controller_methods'
-require 'ceppu/railtie' if defined?(Rails)
+# require 'ceppu/railtie' if defined?(Rails)
+
+module Ceppu
+  class Engine < Rails::Engine
+    initializer 'ceppu.controller_methods' do
+      ActiveSupport.on_load(:action_controller) do
+        include Ceppu::ControllerMethods
+      end
+    end
+  end
+end
