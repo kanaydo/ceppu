cca409f09e0757be20a3941d41886cda99458a0f
kanaydo <batara.girsang@outlook.com>
2025-10-23 21:05:07 +0700

    refactor: clean-up

--- lib/ceppu.rb
+++ lib/ceppu.rb
@@ -7,15 +7,4 @@ if defined?(Rails) # rubocop:disable Style/IfUnlessModifier
 end
 
 module Ceppu
-  # extend Parser
-  # extend Transport
-  #
-  # def self.create_report(exception)
-  #   payload = parse_exeption(exception)
-  #   send_event(payload)
-  # end
-
-  # def self.sample
-  #   raise(StandardError, 'Sample Error')
-  # end
 end
